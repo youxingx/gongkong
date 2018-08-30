4b28cf1b901eff2a0c21bd7980acd53a105584eb
youxingx <1097900172@qq.com>
2018-08-30 11:08:18 +0800

    删除了串口短雷达的注释，添加了GPS，Angle，和长雷达的串口对应的端口号的注释

--- qingshai/init.py
+++ qingshai/init.py
@@ -10,16 +10,13 @@ debug=1
 def main():
 	# 创建一个文件对象，包含对配置文件以及日志文件的处理
 	Log_Cfg=file.File_init(debug=debug)
-	# 创建一个GPS对象，通过串口对gps数据进行处理
+	# 创建一个GPS对象，通过串口对gps数据进行处理  COM4
 	Serial_GPS=serials.Serial_GPS('/dev/ttymxc4')
-	# 创建一个角度对象，通过串口对角度传感器数据进行处理
+	# 创建一个角度对象，通过串口对角度传感器数据进行处理  COM6
 	Serial_Angle=serials.Serial_Angle('/dev/ttyHBB1')
-	# 创建一个短程雷达左对象，通过串口对左边的短程雷达数据进行处理
-	# Serial_LeftRadar=serials.Serial_LeftRadar(13)
-	# 创建一个长程雷达对象，通过串口对长程雷达数据进行处理
+	# 创建一个长程雷达对象，通过串口对长程雷达数据进行处理  COM5
 	Serial_LongRadar=serials.Serial_LongRadar('/dev/ttyHBB0')
-	# 创建一个短程雷达右对象，通过串口对右边的短程雷达数据进行处理
-	# Serial_RightRadar=serials.Serial_RightRadar(15)
+	# 创建短程雷达对象，通过回环网络udp通信对长程雷达数据进行处理  ip:127.0.0.1 port:9000
 	Udp_ShortRadar=shortradar.ShortRadar(port=9000)
 	# 创建一个udp对象，处理udp数据
 	UDP_Server=server.Server_UDP(debug=debug,uip='192.168.183.3',port=4004) #默认本机UDP，match为GPS回环过滤地址（网段）
